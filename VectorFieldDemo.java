25b1e20b9c7fbbb1e9c94d6ce6c83ccf0251e9fe
Neil Thistlethwaite <neilt1111@yahoo.com>
2016-12-08 22:58:25 -0500

    added some standard display stuff

--- VectorFieldDemo.java
+++ VectorFieldDemo.java
@@ -1,12 +1,51 @@
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Math;
 
 
+public class VectorFieldDemo {
 
-public class VectorFieldDemo
-{
    public static void main(String[] args) {
-      System.out.println("placeholder");
-   
+      VFFrame mframe = new VFFrame();
    }
+}
 
+class VFFrame extends JFrame {
 
+   public VFFrame() {
+      super("Vector Field Demo");
+		this.setResizable(false);
+		this.setSize(400,400);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
+   // TODO: check some other jframe code to make control panel,
+   // pack control panel on the right of the vfpanel
+
+   private class VFPanel extends JPanel {
+      
+      public VFPanel() {
+         super();
+      }
+      
+		public Dimension getPreferredSize() {
+			return new Dimension(400,400);
+		}
+   }
 }
